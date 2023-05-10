aba09e421c0b07999507498dbbb51974d02dcd48
it21297854 <99236000+it21297854@users.noreply.github.com>
2023-05-11 00:19:57 +0530

    Commit with login and register
    Commit with login and register

---
+++ Find-Doctor/RealtimedatabaseKotlin/app/src/main/java/com/example/realtimedatabasekotlin/LoginActivity.kt
@@ -0,0 +1,71 @@
+package com.example.realtimedatabasekotlin
+
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.realtimedatabasekotlin.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Login"
+
+        mAuth = FirebaseAuth.getInstance()
+
+        //! Register here
+        binding.tvLoginHere.setOnClickListener{
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
+
+        binding.btnLogin.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val password = binding.etPassword.text.toString()
+            if (username.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Required fields empty..", Toast.LENGTH_SHORT).show()
+            }
+            else if (username.equals("admin") && password.equals("admin")) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+                Toast.makeText(this, "Navigated To Admin Activity", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                binding.progressBar.visibility = View.VISIBLE
+                mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, UserActivity::class.java))
+                        finish()
+                    }
+                    else {
+                        binding.progressBar.visibility = View.GONE
+                        Toast.makeText(this, "Login Failed..", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val user = mAuth.currentUser
+        if (user != null) {
+            startActivity(Intent(this,UserActivity::class.java))
+            //startActivity(Intent(this,AddCourseActivity::class.java))
+            finish()
+        }
+    }
+}
